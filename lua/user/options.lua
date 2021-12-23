67ceabcf61f3266e2419a467edfcb3b61dd181b2
PaulAbloescher <abloescherpaul@gmail.com>
2021-12-23 19:10:21 +0100

    Rewrite options.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,26 +1,27 @@
+local options = {
+    clipboard = "unnamedplus",
+    number = true,
+    relativenumber = true,
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    smartindent = true,
+    hlsearch = true,
+    ignorecase = true,
+    smartcase = true,
+    incsearch = true,
+    errorbells = false,
+    wrap = false,
+    scrolloff = 8,
+    signcolumn = "yes",
+    colorcolumn = "80"
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+
 vim.cmd('language en_US.utf8')
 
-vim.opt.clipboard = "unnamedplus"
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Tab options
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
--- Search options
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-
-vim.opt.errorbells = false
-vim.opt.wrap = false
-
-vim.opt.scrolloff = 8
-
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "80"
