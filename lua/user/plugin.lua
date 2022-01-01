11a072f1503314a2b08efe6922fbfcefee287f83
PaulAbloescher <abloescherpaul@gmail.com>
2022-01-01 20:10:54 +0100

    Add greeter.

--- lua/user/plugin.lua
+++ lua/user/plugin.lua
@@ -84,6 +84,9 @@ return packer.startup(function(use)
   -- Better Terminal Support
   use "akinsho/toggleterm.nvim"
 
+  -- Greeter
+  use 'goolord/alpha-nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
